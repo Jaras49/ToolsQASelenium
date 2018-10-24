87bee70b43291f813b498c8ceb7390df1418cad5
jgodwin <jaras48@gmail.com>
2018-10-24 18:16:11 +0200

    MenuPage impl

---
+++ src/main/java/com/toolsqa/page/menu/MenuPage.java
@@ -0,0 +1,34 @@
+package com.toolsqa.page.menu;
+
+import com.toolsqa.page.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MenuPage extends AbstractPage {
+
+    @FindBy(css = "#primary-menu")
+    private WebElement primaryMenu;
+
+    @FindBy(xpath = "//*[@id='primary-menu']//*[contains(text(), 'DEMO SITES')]")
+    private WebElement demoSites;
+
+    @FindBy(xpath = "//*[@id='primary-menu']//*[contains(text(), 'Automation Practice Form')]")
+    private WebElement automationPracticeForm;
+
+    public MenuPage(WebDriver driver, WebDriverWait wait, Actions actions) {
+        super(driver, wait, actions);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void moveToDemoSites() {
+        actions.moveToElement(waitUntilClickable(demoSites)).perform();
+    }
+
+    public void goToAutomationFormPage() {
+        waitUntilClickable(automationPracticeForm).click();
+    }
+}
