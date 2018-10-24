279eda1f4234d3423857c214cf176ff2ceb8a95f
jgodwin <jaras48@gmail.com>
2018-10-24 18:12:48 +0200

    added wait methods

--- src/main/java/com/toolsqa/page/AbstractPage.java
+++ src/main/java/com/toolsqa/page/AbstractPage.java
@@ -1,8 +1,12 @@
 package com.toolsqa.page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public abstract class AbstractPage {
 
@@ -10,9 +14,17 @@ public abstract class AbstractPage {
     protected WebDriverWait wait;
     protected Actions actions;
 
-    public AbstractPage(WebDriver driver, WebDriverWait wait, Actions actions) {
+    protected AbstractPage(WebDriver driver, WebDriverWait wait, Actions actions) {
         this.driver = driver;
         this.wait = wait;
         this.actions = actions;
+    }
+
+    protected WebElement waitUntilClickable(WebElement webElement) {
+        return wait.until(ExpectedConditions.elementToBeClickable(webElement));
+    }
+
+    protected List<WebElement> waitUntilVisible(List<WebElement> webElements) {
+        return wait.until(ExpectedConditions.visibilityOfAllElements(webElements));
     }
 }
