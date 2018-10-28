5160a947f0147dfd0a8692e15ab2df594d03e3c1
jgodwin <jaras48@gmail.com>
2018-10-28 14:26:00 +0100

    WaitUntilVisible annotation impl.

--- src/main/java/com/toolsqa/page/AbstractPage.java
+++ src/main/java/com/toolsqa/page/AbstractPage.java
@@ -1,12 +1,15 @@
 package com.toolsqa.page;
 
+import com.toolsqa.annotation.WaitUntilVisible;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,5 +45,31 @@ public abstract class AbstractPage {
         ArrayList<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(windowHandles.get(index));
         driver.manage().window().maximize();
+    }
+
+    public <T, M> T assertEquals(M actual, M expected, T pageObject) {
+        Assert.assertEquals(actual, expected);
+        return pageObject;
+    }
+
+    protected void waitUntilPageLoads() {
+        wait.until(ExpectedConditions.visibilityOfAllElements(getFieldsAnnotatedWithWaitForElement()));
+    }
+
+    private List<WebElement> getFieldsAnnotatedWithWaitForElement() {
+        List<WebElement> elements = new ArrayList<>();
+        try {
+            for (Field declaredField : this.getClass().getDeclaredFields()) {
+                declaredField.setAccessible(true);
+                if (declaredField.getType() == WebElement.class && declaredField.isAnnotationPresent(WaitUntilVisible.class)) {
+                    elements.add(((WebElement) declaredField.get(this)));
+                }
+                declaredField.setAccessible(false);
+            }
+
+        } catch (IllegalAccessException ex) {
+            System.err.println(ex);
+        }
+        return elements;
     }
 }
